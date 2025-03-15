ea0ce19b3b1fe6e5d0123c7ed1a03fd910b3369b
Yonatan Girmachew <yonatangirmachew3@gmail.com>
2025-03-16 00:03:51 +0300

    backend boilerplate

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from  "express"
+import cors from "cors"
+import 'dotenv/config'
+import cookieParser from "cookie-parser"
+
+const app = express();
+const port = process.env.PORT || 4000
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials:true}))
+
+
+app.get('/',(req,res)=>res.send("API working!"))
+
+app.listen(port,()=> console.log(`server running on http://localhost:${port}`));

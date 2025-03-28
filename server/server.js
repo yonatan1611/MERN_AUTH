efcd5dcf7ff77343c918833ca5a6355b29a38413
Yonatan Girmachew <yonatangirmachew3@gmail.com>
2025-03-28 21:23:26 +0300

    userData fetch controller function setup

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import cookieParser from "cookie-parser"
 import ConnectDB from "./config/mongodb.js";
 import authRouter from './routes/authRoutes.js'
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000    
@@ -16,5 +17,6 @@ app.use(cors({credentials:true}))
 //API Endpoints
 app.get('/',(req,res)=>res.send("API working"));
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port,()=> console.log(`server running on http://localhost:${port}`));

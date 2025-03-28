9f746b67564e2d0d774b6a9367958fbb3d0ab55c
Yonatan Girmachew <yonatangirmachew3@gmail.com>
2025-03-28 22:11:30 +0300

    react router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import ResetPassword from './pages/resetPassword'
+import Login from './pages/login'
+import EmailVerify from './pages/emailVerify'
+import Home from './pages/home'
 
 const App = () => {
   return (
-    <div className='text-4xl'>
-      App
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/email-verify' element={<EmailVerify/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+      </Routes>
       </div>
   )
 }
---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmailVerify = () => {
+  return (
+    <div>
+        <h1>Email Verify</h1>
+    </div>
+  )
+}
+
+export default EmailVerify;
